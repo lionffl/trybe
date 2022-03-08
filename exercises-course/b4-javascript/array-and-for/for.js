5b11cddecc4e64139b006c96aa2fbc7fd9f975e8
Felipe <lionffl@gmail.com>
2022-03-08 16:09:12 -0300

    for-of exercise added

---
+++ exercises-course/b4-javascript/array-and-for/for.js
@@ -0,0 +1,7 @@
+// Use the for loop to print the elements of the following array:
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (let i = 0; i < groceryList.length; i++) {
+    console.log(groceryList[i])
+}
