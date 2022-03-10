9f0632f17a97128a8238a9140b881f9897facf75
Felipe <lionffl@gmail.com>
2022-03-10 18:24:05 -0300

    exercises.js concluded

--- exercises-course/b4-javascript/logic-programming-and-algorithm/exercises.js
+++ exercises-course/b4-javascript/logic-programming-and-algorithm/exercises.js
@@ -36,6 +36,46 @@ console.log(newWord);
 
 let languages = ['java', 'javascript', 'python', 'html', 'css'];
 
+let charMax = 0;
+let charMin = Number.MAX_SAFE_INTEGER;
+let biggerWord = "";
+let smallerWord = "";
 
+for (let i = 0; i < languages.length; i++) {
+    if (languages[i].length > charMax) {
+        charMax = languages[i].length;
+        biggerWord = languages[i];
+    } 
+    else if (languages[i].length < charMin) {
+        charMin = languages[i].length;
+        smallerWord = languages[i];
+    }
+}
+
+console.log(`Smaller word: ${smallerWord}`);
+console.log(`Bigger word: ${biggerWord}`);
 
 // Print the higher prime number between 0 and 50
+
+let prime;
+
+for (let i = 50; i >= 0; i--) {
+    let counter = 0;    
+    for (let j = 0; j <= 50; j++) {
+        if (i % j === 0) {
+            counter++;
+        }
+    }
+    if (counter === 2) { 
+        prime = i;
+        break;
+    }
+}
+
+console.log(prime);
+
+
+
+
+
+
