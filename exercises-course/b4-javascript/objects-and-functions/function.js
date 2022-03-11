5b0c3644e3bf19815e653c2d2a1d2c6353f545b1
Felipe <lionffl@gmail.com>
2022-03-11 14:13:16 -0300

    function.js update

---
+++ exercises-course/b4-javascript/objects-and-functions/function.js
@@ -0,0 +1,43 @@
+// Write 5 functions for each exercise from 4.1 section:
+
+// const a = 10;
+// const b = 5;
+
+// +
+
+// result = a + b;
+// console.log(result);
+
+// -
+
+// result = a - b;
+// console.log(result);
+
+// *
+
+// result = a * b;
+// console.log(result);
+
+// /
+
+// result = a / b;
+// console.log(result);
+
+// %
+
+// result = a % b;
+// console.log(result);
+
+
+let a = 10;
+let b = 5;
+let op = "";
+
+
+function toCalc(number1, number2, operation)  {
+    switch (operation) {
+        case "sum":
+    }
+    
+    return number1 + number2;
+}
