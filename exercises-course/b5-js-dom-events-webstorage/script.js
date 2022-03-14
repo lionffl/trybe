0be8483033cfa8b25c6b1d8b44d5216abb0fa1b3
Felipe <lionffl@gmail.com>
2022-03-14 19:38:19 -0300

    block 5 start

---
+++ exercises-course/b5-js-dom-events-webstorage/script.js
@@ -0,0 +1,16 @@
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+
+// 1)
+
+document.getElementById('page-title').innerText = "Arcane.";
+document.getElementById('second-paragraph').innerText = "Best video game series of all time.";
+document.getElementById('subtitle').innerText = "A League of Legends story";
+
+// 2)
+
+document.getElementsByClassName('content')[0].style.textDecoration = 'underline';
+
+// 3)
+
+document.getElementsByTagName('h4')[0].style.color = 'red';
