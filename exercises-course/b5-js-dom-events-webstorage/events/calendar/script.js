ff152695877081c66e9512701271e64ff7235e2f
Felipe <lionffl@gmail.com>
2022-03-17 12:10:54 -0300

    calendar 1) added

--- exercises-course/b5-js-dom-events-webstorage/events/calendar/script.js
+++ exercises-course/b5-js-dom-events-webstorage/events/calendar/script.js
@@ -14,3 +14,31 @@ function createDaysOfTheWeek() {
   createDaysOfTheWeek();
   
   // Escreva seu código abaixo.
+
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+const ulDays = document.getElementById('days');
+
+// 1)
+
+for (let i = 0; i < dezDaysList.length; i++) {
+  const liDay = document.createElement('li');
+  ulDays.appendChild(liDay);
+
+  if (dezDaysList[i] === 24 || dezDaysList[i] === 31) {
+    liDay.className = 'day holiday';
+    liDay.innerText = dezDaysList[i];
+  }
+  else if (dezDaysList[i] === 4 || dezDaysList[i] === 11 || dezDaysList[i] === 18 ) {
+    liDay.className = 'day friday';
+    liDay.innerText = dezDaysList[i];
+  }
+  else if (dezDaysList[i] === 25 ) {
+    liDay.className = 'day friday holiday';
+    liDay.innerText = dezDaysList[i];
+  } else {
+    liDay.className = 'day';
+    liDay.innerText = dezDaysList[i];
+  }
+}
+
