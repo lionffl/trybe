4d4eb70d2e01d65e6d162d7f396bff131d686b2e
Felipe <lionffl@gmail.com>
2022-04-07 20:31:06 -0300

    exercises update

--- exercises-course/b8-high-order-functions/intro-to-hof/script.js
+++ exercises-course/b8-high-order-functions/intro-to-hof/script.js
@@ -8,7 +8,7 @@ const doingThings = (a) => console.log(a());
 
 doingThings(wakeUp);
 
-const newEmployees = (a,b,c) => {
+const newEmployees = (a, b, c) => {
   const employees = {
     id1: a, // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
     id2: b, // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
@@ -17,7 +17,7 @@ const newEmployees = (a,b,c) => {
   return employees;
 };
 
-const addEmployee = (name) => ({ name, email: `${name.toLowerCase().replace(/ /g,'_')}@trybe.com` })
+const addEmployee = (name) => ({ name, email: `${name.toLowerCase().replace(/ /g, '_')}@trybe.com` })
 
 console.log(newEmployees(addEmployee('Felipe Lima'), addEmployee('Joao Batista'), addEmployee('Maria das Gracas')));
 
@@ -30,5 +30,31 @@ const gameGen = (myGame, winCondition) => {
   return winCondition(myGame, number) ? 'Win!' : 'Lose!'
 }
 
-console.log(gameGen(4,winCondition));
+console.log(gameGen(4, winCondition));
 
+
+const checkAnswer = (RIGHT_ANSWERS, STUDENT_ANSWERS) => {
+  let grade = 0;
+  for (let i = 0; i < 10; i += 1) {
+    if (STUDENT_ANSWERS[i] === 'N.A') {
+      grade = grade;
+    } else {
+      switch (RIGHT_ANSWERS[i] === STUDENT_ANSWERS[i]) {
+        case true:
+          grade += 1;
+          break;
+        case false:
+          grade -= 0.5;
+        default:
+          grade = grade;
+          break;
+      }
+    }
+  }
+  return grade;
+}
+
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+console.log(checkAnswer(RIGHT_ANSWERS,STUDENT_ANSWERS));
