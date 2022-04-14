8a59f69dedecfe837d9ebce42bb748314665a317
Felipe <lionffl@gmail.com>
2022-04-14 10:33:10 -0300

    exercises added

--- exercises-course/m1-web-dev-fundamentals/b8-high-order-functions/spread-operator-rest-destructuring/script.js
+++ exercises-course/m1-web-dev-fundamentals/b8-high-order-functions/spread-operator-rest-destructuring/script.js
@@ -13,3 +13,81 @@ const fruitSalad = (fruit, additional) => {
 };
 
 console.log(fruitSalad(specialFruit, additionalItens));
+
+
+function quantosParams(...args) {
+  console.log('parâmetros:', args);
+  return `Você passou ${args.length} parâmetros para a função.`;
+}
+
+console.log(quantosParams(0, 1, 2)); // Você passou 3 parâmetros para a função.
+console.log(quantosParams('string', null, [1, 2, 3], { })); // Você passou 4 parâmetros para a função.
+
+const user = {
+  username: 'Maria',
+  age: 21,
+  nationality: 'Brazilian',
+};
+
+const jobInfos = {
+  profession: 'Software engineer',
+  squad: 'Rocket Landing Logic',
+  squadInitials: 'RLL',
+};
+
+const userJobInfos = {
+  ...user,
+  ...jobInfos,
+}
+
+console.log(userJobInfos);
+
+const { username, age, nationality, profession, squad, squadInitials } = userJobInfos;
+
+console.log(`Hi, my name is ${username}, I'm ${age} years old and I'm ${nationality}. I work as a ${profession} and my squad is ${squadInitials}-${squad}`)
+
+const saudacoes = ['Olá', (saudacao) => console.log(saudacao)];
+
+saudacoes[1](saudacoes[0]); // Olá
+
+// Produza o mesmo resultado acima, porém utilizando array destructuring
+
+const [ string, randomFunction ] = saudacoes;
+
+randomFunction(string);
+
+let comida = 'gato';
+let animal = 'água';
+let bebida = 'arroz';
+
+console.log(comida, animal, bebida); // arroz gato água
+
+// Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
+
+[comida, animal, bebida] = [bebida, comida, animal];
+
+
+console.log(comida, animal, bebida); // arroz gato água
+
+let numerosPares = [1, 3, 5, 6, 8, 10, 12];
+
+console.log(numerosPares); // [6, 8, 10, 12];
+
+// Utilize array destructuring para produzir o resultado esperado pelo console.log abaixo
+
+[,,, ...numerosPares] = numerosPares;
+
+console.log(numerosPares); // [6, 8, 10, 12];
+
+const getPosition = (latitude, longitude) => ({
+  latitude,
+  longitude,
+});
+
+console.log(getPosition(-19.8157, -43.9542));
+
+const multiply = (number, value = 1) => {
+  return number * value;
+};
+
+console.log(multiply(8));
