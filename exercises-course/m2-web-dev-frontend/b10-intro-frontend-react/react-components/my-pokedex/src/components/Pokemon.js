6764718123d28b448a0d998701d8044634ad04c6
Felipe <lionffl@gmail.com>
2022-05-09 08:22:08 -0300

    pokedex proptypes added

--- exercises-course/m2-web-dev-frontend/b10-intro-frontend-react/react-components/my-pokedex/src/components/Pokemon.js
+++ exercises-course/m2-web-dev-frontend/b10-intro-frontend-react/react-components/my-pokedex/src/components/Pokemon.js
@@ -1,21 +1,37 @@
 import React from "react";
-import getPokemonInfo from "../functions/getPokemonInfo";
+import PropTypes from 'prop-types';
 
 class Pokemon extends React.Component {
-  render(){
-
-    const pokemon = this.props.pokemon;
+  
+  getPokemonInfo = (moreInfo) => window.open(moreInfo, '_blank');
+  
+  render() {
+    
+    const pokemon = this.props.pokemon
+    const { name, type, image, moreInfo } = pokemon;
     const { value, measurementUnit } = pokemon.averageWeight;
 
     return (
-    <article className="pokemon">
-      <img src={pokemon.image} alt={pokemon.name} onClick={getPokemonInfo}></img>
-      <h4>{pokemon.name}</h4>
-      <p>Element/Type: {pokemon.type}</p>
-      <p>Weight: {value} {measurementUnit}</p>
-    </article>
+      <article className="pokemon">
+        <img src={image} alt={name} onClick={() => this.getPokemonInfo(moreInfo)} />
+        <h4>{name}</h4>
+        <p>Element/Type: {type}</p>
+        <p>Weight: {value} {measurementUnit}</p>
+      </article>
     )
   }
+}
+
+Pokemon.propTypes = {
+  pokemon: PropTypes.shape({
+    image: PropTypes.string,
+    name: PropTypes.string,
+    type: PropTypes.string,
+    averageWeight: PropTypes.shape({
+      value: PropTypes.number,
+      measurementUnit: PropTypes.string
+    })
+  })
 }
 
 export default Pokemon;
--- exercises-course/m2-web-dev-frontend/b10-intro-frontend-react/react-components/my-pokedex/src/functions/getPokemonInfo.js
+++
@@ -1,3 +0,0 @@
-const getPokemonInfo = (pokemon) => window.open(`https://bulbapedia.bulbagarden.net/wiki/${pokemon.target.alt}`, '_blank');
-
-export default getPokemonInfo;
