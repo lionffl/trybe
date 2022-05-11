6c3dc4b399d7c15b2d5b68daa832a779e12ab961
Felipe <lionffl@gmail.com>
2022-05-11 16:20:21 -0300

    pokedex update

--- exercises-course/m2-web-dev-frontend/b10-intro-frontend-react/react-components/my-pokedex/src/components/Pokedex.js
+++ exercises-course/m2-web-dev-frontend/b10-intro-frontend-react/react-components/my-pokedex/src/components/Pokedex.js
@@ -4,12 +4,24 @@ import pokemons from '../data'
 
 
 class Pokedex extends React.Component {
+  
+  state = {
+    pokemonIndex: 0,
+  }
+
+  nextPokemon = () => {
+    if (this.state.pokemonIndex >= pokemons.length-1) this.setState({ pokemonIndex: 0 });
+    else { this.setState({ pokemonIndex: this.state.pokemonIndex + 1 }) };
+}
   render() {
     const pokedex = pokemons.map((pokemon) => <Pokemon key={pokemon.id} pokemon={pokemon} />);
     return (
       <>
         <h1>Pokedex</h1>
-        <section className="pokedex">{pokedex}</section>
+        <section className="pokedex">
+          {pokedex[this.state.pokemonIndex]}
+          <button onClick={this.nextPokemon} className="next-btn">Next Pokemon</button>
+        </section>
       </>
     )
   }
