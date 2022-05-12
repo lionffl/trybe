d2e7e3d0ad123605260b7adc281cef3e4f57cef4
Felipe <lionffl@gmail.com>
2022-05-12 13:05:37 -0300

    css update a Normal filter removed

--- exercises-course/m2-web-dev-frontend/b10-intro-frontend-react/react-components/my-pokedex/src/components/Buttons.js
+++ exercises-course/m2-web-dev-frontend/b10-intro-frontend-react/react-components/my-pokedex/src/components/Buttons.js
@@ -10,7 +10,6 @@ class Buttons extends Component {
           <button onClick={setFilterFunction} className="btn" name="">No filter</button>
           <button onClick={setFilterFunction} className="btn" name="Fire">Only Fire</button>
           <button onClick={setFilterFunction} className="btn" name="Psychic">Only Psychic</button>
-          <button onClick={setFilterFunction} className="btn" name="Normal">Only Normal</button>
           <button onClick={setFilterFunction} className="btn" name="Poison">Only Poison</button>
           <button onClick={setFilterFunction} className="btn" name="Electric">Only Electric</button>
         </section>
