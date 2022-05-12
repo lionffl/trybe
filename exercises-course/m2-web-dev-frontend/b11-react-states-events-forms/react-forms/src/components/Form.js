d326b443db343bf123144c5fbe276806638a6a98
Felipe <lionffl@gmail.com>
2022-05-12 17:26:57 -0300

    form added

---
+++ exercises-course/m2-web-dev-frontend/b11-react-states-events-forms/react-forms/src/components/Form.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+
+class Form extends Component {
+  state = {
+    inputName: '',
+    inputEmail: '',
+    favoriteGame: '',
+    textAreaInput: ''
+  }
+
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  render() {
+    const { inputEmail, inputName, textAreaInput } = this.state;
+    return (
+      <div className='form-container'>
+        <form className='form'>
+          <h2>A Simple Form</h2>
+          <label>
+            Name: <br />
+            <input type="text" name="inputName" value={inputName} onChange={this.handleChange} />
+          </label>
+          <label>
+            Email: <br />
+            <input name="inputEmail" type="email" value={inputEmail} onChange={this.handleChange}/>
+          </label>
+          <label>
+            Favorite Game Genre:<br />
+            <select name="favoriteGame" onChange={this.handleChange}>
+              <option value="RTS">RTS</option>
+              <option value="FPS">FPS</option>
+            </select>
+          </label>
+          <label>
+            Suggestions:<br />
+            <textarea name="textAreaInput" resize="none" cols="25" rows="5" value={textAreaInput} onChange={this.handleChange} />
+          </label>
+          <button>Submit</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Form
