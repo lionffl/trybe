438aabe30df467f49630fdb056f30e71fa57d3ec
Felipe <lionffl@gmail.com>
2022-08-23 20:00:13 -0300

    chocolates started

---
+++ exercises-course/m3-web-dev-backend/b22-intro-to-node/integration-tests/cacao-trybe/tests/integration/chocolates.test.js
@@ -0,0 +1,21 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Usando o método GET em /chocolates', function () {
+  it('Retorna a lista completa de chocolates!', async function () {
+      const response = await chai
+        .request(app)
+        .get('/chocolates');
+      const output = [
+        { id: 1, name: 'Mint Intense', brandId: 1 },
+        { id: 2, name: 'White Coconut', brandId: 1 },
+        { id: 3, name: 'Mon Chéri', brandId: 2 },
+        { id: 4, name: 'Mounds', brandId: 3 },
+      ];
+      expect(response.body.chocolates).to.deep.equal(output);    
+  });
+});
