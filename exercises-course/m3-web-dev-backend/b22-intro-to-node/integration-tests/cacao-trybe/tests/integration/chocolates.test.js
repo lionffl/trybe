7c9cb5d2b0a635643cdc019225ea6cbb8cda1a22
Felipe <lionffl@gmail.com>
2022-08-24 10:14:31 -0300

    cacaoTrybe added

--- exercises-course/m3-web-dev-backend/b22-intro-to-node/integration-tests/cacao-trybe/tests/integration/chocolates.test.js
+++ exercises-course/m3-web-dev-backend/b22-intro-to-node/integration-tests/cacao-trybe/tests/integration/chocolates.test.js
@@ -1,21 +1,78 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const app = require('../../src/app');
+const sinon = require('sinon');
+const fs = require('fs');
+const mockFile = require('../mocks/mockFile');
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('Usando o método GET em /chocolates', function () {
-  it('Retorna a lista completa de chocolates!', async function () {
-      const response = await chai
-        .request(app)
-        .get('/chocolates');
+describe('Testando a API Cacao Trybe', function () {
+  beforeEach(function () {
+    sinon.stub(fs.promises, 'readFile')
+      .resolves(mockFile);
+  });
+
+  afterEach(function () {
+    sinon.restore();
+  });
+
+  describe('Usando o método GET em /chocolates', function () {
+    it('Retorna a lista completa de chocolates!', async function () {
       const output = [
         { id: 1, name: 'Mint Intense', brandId: 1 },
         { id: 2, name: 'White Coconut', brandId: 1 },
         { id: 3, name: 'Mon Chéri', brandId: 2 },
         { id: 4, name: 'Mounds', brandId: 3 },
       ];
-      expect(response.body.chocolates).to.deep.equal(output);    
+
+      const response = await chai.request(app).get('/chocolates');
+      expect(response.status).to.be.equal(200);
+      expect(response.body.chocolates).to.deep.equal(output);
+    });
+  });
+
+  describe('Usando o método GET em /chocolates/:id para buscar o ID 4', function () {
+    it('Retorna o chocolate Mounds', async function () {
+      const response = await chai
+        .request(app)
+        .get('/chocolates/4');
+
+      const output = [
+        {
+          id: 4,
+          name: 'Mounds',
+          brandId: 3,
+        }];
+
+      expect(response.status).to.be.equal(200);
+      expect(response.body.chocolate).to.deep.equal(output);
+    });
+  });
+
+  describe('Usando o método GET em /chocolates/brand/:brandId para buscar brandId 1', function () {
+    it('Retorna os chocolates da marca Lindt & Sprungli', async function () {
+      const response = await chai
+        .request(app)
+        .get('/chocolates/brand/1');
+
+      const output = [
+        {
+          id: 1,
+          name: 'Mint Intense',
+          brandId: 1,
+        },
+        {
+          id: 2,
+          name: 'White Coconut',
+          brandId: 1,
+        },
+      ];
+
+      expect(response.status).to.be.equal(200);
+      expect(response.body.chocolates).to.deep.equal(output);
+    });
   });
 });
