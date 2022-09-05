afada085f0ce58c6c8504ada9eb65264eb2b7cd4
Felipe <lionffl@gmail.com>
2022-09-05 10:47:44 -0300

    update and delete functionality person added

--- exercises-course/m3-web-dev-backend/b22-intro-to-node/express-sql/trybecash-api/src/controller/peopleController.js
+++ exercises-course/m3-web-dev-backend/b22-intro-to-node/express-sql/trybecash-api/src/controller/peopleController.js
@@ -2,6 +2,8 @@ const {
   insert,
   findAll,
   findById,
+  update,
+  remove,
 } = require('../db/peopleDB');
 
 const createPerson = async (req, res) => {
@@ -44,8 +46,39 @@ const getPersonById = async (req, res) => {
   }
 };
 
+const updatePerson = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const person = req.body;
+    const [result] = await update(person, id);
+    if (result.affectedRows > 0) {
+      res.status(200).json({ message: `Pessoa de id ${id} atualizada com sucesso` });
+    } else {
+      res.status(404).json({ message: 'Pessoa não encontrada' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.sqlMessage });
+  }
+};
+
+const deletePerson = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const [result] = await remove(id);
+    if (result.affectedRows > 0) {
+      res.status(200).json({ message: `Pessoa de id ${id} excluída com sucesso` });
+    } else {
+      res.status(404).json({ message: 'Pessoa não encontrada' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.sqlMessage });
+  }
+};
+
 module.exports = {
   createPerson,
   getPeople,
   getPersonById,
+  updatePerson,
+  deletePerson,
 }
--- exercises-course/m3-web-dev-backend/b22-intro-to-node/express-sql/trybecash-api/src/routes/peopleRoutes.js
+++ exercises-course/m3-web-dev-backend/b22-intro-to-node/express-sql/trybecash-api/src/routes/peopleRoutes.js
@@ -3,6 +3,8 @@ const {
   createPerson,
   getPeople,
   getPersonById,
+  updatePerson,
+  deletePerson,
  } = require('../controller/peopleController');
 
 const router = express.Router();
@@ -10,9 +12,10 @@ const router = express.Router();
 // router.get('/', getPeople);
 // router.post('/', createPerson);
 // router.get('/:id', getPersonById);
+// router.put('/:id', updatePerson);
+// router.put('/:id', deletePerson);
 
 router.route('/').get(getPeople).post(createPerson);
-router.route('/:id').get(getPersonById);
-
+router.route('/:id').get(getPersonById).put(updatePerson).delete(deletePerson);
 
 module.exports = router;
