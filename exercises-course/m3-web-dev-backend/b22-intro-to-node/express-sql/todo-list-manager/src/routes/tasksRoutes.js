1c8e3b53b9f0bbdd6bd345d6f0e5962325151eea
Felipe <lionffl@gmail.com>
2022-09-05 18:47:10 -0300

    routes reorganized

--- exercises-course/m3-web-dev-backend/b22-intro-to-node/express-sql/todo-list-manager/src/routes/tasksRoutes.js
+++ exercises-course/m3-web-dev-backend/b22-intro-to-node/express-sql/todo-list-manager/src/routes/tasksRoutes.js
@@ -10,14 +10,13 @@ const {
   getTaskById,
 } = require('../controller/taskController');
 
-router.post('/', createTask);
+// router.post('/', createTask);
+// router.put('/:id', updateTask);
+// router.delete('/:id', deleteTask);
+// router.get('/', getTasks);
+// router.get('/:id', getTaskById);
 
-router.put('/:id', updateTask);
-
-router.delete('/:id', deleteTask);
-
-router.get('/', getTasks);
-
-router.get('/:id', getTaskById);
+router.route('/').get(getTasks).post(createTask);
+router.route('/:id').put(updateTask).delete(deleteTask).get(getTaskById);
 
 module.exports = router;
