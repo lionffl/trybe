4fc327007ce1f8f1ead8df26d3940f109d421d3a
Felipe <lionffl@gmail.com>
2022-10-04 15:10:19 -0300

    interfaces added

---
+++ exercises-course/m3-web-dev-backend/s08-typescript/static-typing-generic/interfaces.ts
@@ -0,0 +1,40 @@
+interface Car {
+  brand: string
+  year: number
+  model: string
+  type: string
+}
+
+interface Feline {
+  race: string
+  age: number
+  speed: number
+}
+
+interface Aircraft {
+  type: string
+  year: number
+  speed: number
+}
+
+interface Cat extends Feline {
+  name: string
+  play(): string
+}
+
+const car1: Car = {
+  brand: 'fiat',
+  year: 2015,
+  model: "siena",
+  type: "sedan"
+}
+
+const cat1: Cat = {
+  name: 'Dinha',
+  play: function (): string {
+    return `${this.name} is playing`
+  },
+  race: 'persa',
+  age: 3,
+  speed: 20
+}
