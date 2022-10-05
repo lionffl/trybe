8db0a21045693a51c5dbf2fb8d413359f55cc449
Felipe <lionffl@gmail.com>
2022-10-05 13:24:41 -0300

    types added to intermediate variables

--- exercises-course/m3-web-dev-backend/s08-typescript/static-typing-generic/exercises/myFilter.ts
+++ exercises-course/m3-web-dev-backend/s08-typescript/static-typing-generic/exercises/myFilter.ts
@@ -1,10 +1,10 @@
-type callback<T> = (item: T, index?: number, array?: T[]) => boolean;
+type callback<T> = (element: T, index?: number, array?: T[]) => boolean;
 
 function myFilter<T>(array: T[], fn: callback<T>): T[] {
-  const filtredArray = [];
+  const filtredArray: T[] = [];
 
   for (let index = 0; index < array.length; index++) {
-    const element = array[index];
+    const element: T = array[index];
 
     if (fn(element, index, array)) {
       filtredArray.push(element);  
