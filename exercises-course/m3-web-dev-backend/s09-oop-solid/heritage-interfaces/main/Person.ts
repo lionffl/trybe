918fe9f68d618d3dd47e7a8da84bd04eff5592b0
Felipe <lionffl@gmail.com>
2022-10-11 18:22:40 -0300

    heritage Person.ts exercises added

--- exercises-course/m3-web-dev-backend/s09-oop-solid/heritage-interfaces/main/Person.ts
+++ exercises-course/m3-web-dev-backend/s09-oop-solid/heritage-interfaces/main/Person.ts
@@ -1,3 +1,5 @@
+import { throws } from "assert";
+
 class Person {
   name: string;
   address: string;
@@ -25,3 +27,40 @@ export class LegalPerson extends Person {
     this.cnpj = cnpj;
   }
 }
+
+class Superclass {
+  public isSuper: boolean = true;
+
+  sayHello() {}
+}
+
+class Subclass extends Superclass {
+  constructor() {
+    super()
+    this.isSuper = false;
+  }
+  sayHello(): void {
+    console.log('hello from subclass')
+  }
+}
+
+class Subclass2 extends Superclass {
+  sayHello(): void {
+    console.log('hello from subclass 2')
+  }
+}
+
+const obj = new Superclass();
+const obj2 = new Subclass();
+const obj3 = new Subclass2();
+
+function myFunc(obj: Superclass) {
+  if (obj.isSuper) {
+    console.log('Super!')
+  } else { console.log('Sub!') }
+}
+
+myFunc(obj);
+myFunc(obj2);
+myFunc(obj3);
+
